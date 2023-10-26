6f7132a4b439478914de182db5b42ed54fdc082e
Sumesh <sgaud4@gmail.com>
2023-10-26 13:42:40 -0500

    Testing 1

--- AzureAirlines/AutoCommit.cs
+++ AzureAirlines/AutoCommit.cs
@@ -3,11 +3,11 @@ using LibGit2Sharp;
 
 public class AutoCommit
 {
-    //private readonly string _repository = @"https://github.com/benjaminsampica/azureairlines.git";
+    
     private readonly string _repository = @"C:\Users\sg57737\Developer\AzureAirlines";
     public void Commit()
     {
-        var message = "Testing";
+        var message = "Testing 1";
 
         using(var repo = new Repository(_repository))
         {
