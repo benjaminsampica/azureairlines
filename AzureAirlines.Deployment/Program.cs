f3eed06695640ffd2bb0e097225a563ca5b661c5
Ben Sampica <684314@corp.caseys.com>
2023-10-26 13:59:44 -0500

    hi

--- AzureAirlines.Deployment/Program.cs
+++ AzureAirlines.Deployment/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Pulumi;
-using Pulumi.AzureAD;
-using Pulumi.AzureNative.Authorization;
+//using Pulumi.AzureAD;
+//using Pulumi.AzureNative.Authorization;
 using Pulumi.AzureNative.Resources;
 
 await Pulumi.Deployment.RunAsync<DeployStack>();
@@ -15,12 +15,12 @@ public class DeployStack : Stack
             Location = "North Central US",
         });
 
-        var servicePrincipal = new ServicePrincipal("serviceprincipaltest");
+        //var servicePrincipal = new ServicePrincipal("serviceprincipaltest");
 
-        var roleAssignment = new RoleAssignment("roleassignmenttest", new RoleAssignmentArgs
-        {
-            PrincipalId = servicePrincipal.Id,
-            Scope = resourceGroup.Id
-        });
+        //var roleAssignment = new RoleAssignment("roleassignmenttest", new RoleAssignmentArgs
+        //{
+        //    PrincipalId = servicePrincipal.Id,
+        //    Scope = resourceGroup.Id
+        //});
     }
 }
