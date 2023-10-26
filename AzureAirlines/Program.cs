c9bc17dc3642f8119081661b705f144c69238f87
James Sampica <jtsampica@gmail.com>
2023-10-26 13:19:18 -0500

    form work

--- AzureAirlines/Program.cs
+++ AzureAirlines/Program.cs
@@ -7,8 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddHostedService<CreateAzureResourceWorker>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
