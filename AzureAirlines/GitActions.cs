ad48f1e40dd7ee179eaf33a6eaf9ed3d7cfc0233
Sumesh Gaud <sg57737@corp.caseys.com>
2023-10-27 08:47:03 -0500

    Chnages

---
+++ AzureAirlines/GitActions.cs
@@ -0,0 +1,21 @@
+using Octokit;
+
+public class GitAction
+{
+    private readonly string _owner = "benjaminsampica";
+    private readonly string _repoName = "azureairlines";
+    private readonly string _token = "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";
+
+    public async Task UploadFileAsync(string fileContent, string fileName)
+    {
+        var github = new GitHubClient(new ProductHeaderValue(_owner));
+        github.Credentials = new Credentials(_token);
+
+        var repo = await github.Repository.Get(_owner, _repoName);
+        string filePath = $"{_owner}/{_repoName}/AzureAirlines/{fileName}";
+
+        var createFileRequest = new CreateFileRequest("new file " +  fileName, fileContent, "main", true);
+        var newFile = await github.Repository.Content.CreateFile(owner: _owner, _repoName, filePath, createFileRequest);
+
+    }
+}
